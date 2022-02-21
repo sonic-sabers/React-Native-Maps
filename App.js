186623d49626793be49658fc5c0837057c162b46
Ashish <ofb911@gmail.com>
2022-02-21 23:23:22 +0530

    Maps

---
+++ App.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text, 
+  useColorScheme,
+  View,
+} from 'react-native';
+;
+
+export default App = () => {
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <Text>sdfvadf</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+});
+
